924449d2bc345c5ddf4d3ff870c4af498447440c
Qu1etboy <nonzagreanthai@gmail.com>
2023-01-16 22:50:00 +0700

    feat(*): initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Resume Edit</title>
+        <meta
+          name="description"
+          content="Create your resume with ease. type in your information and download as PDF"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </>
+  );
+}
