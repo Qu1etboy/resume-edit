1bc83308cd5ed15701e650138350179fb4ad09e8
Qu1etboy <nonzagreanthai@gmail.com>
2023-01-21 22:00:05 +0700

    style: ✨design a landing page

---
+++ components/home/CTA.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { ResumeLogo } from "../Graphic";
+
+export default function CallToAction() {
+  return (
+    <section className="bg-white">
+      <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+        <ResumeLogo />
+        <div className="mt-4 md:mt-0">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
+            Start creating your resume today.
+          </h2>
+          <p className="mb-6 font-light text-gray-500 md:text-lg">
+            Built your amazing resume. Apply for the job you want without time
+            wasted on the design and the idea.
+          </p>
+          <Link
+            href="/edit"
+            className="inline-flex items-center text-white bg-teal-500 hover:bg-teal-600 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            Create your resume
+            <svg
+              className="ml-2 -mr-1 w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ components/home/Footer.tsx
@@ -0,0 +1,78 @@
+export default function Footer() {
+  return (
+    <footer className="px-4 pb-4 pt-16 bg-gray-900 text-gray-100 sm:px-6 sm:pb-6 border-t">
+      <div className="mx-auto max-w-screen-xl">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <h3 className="flex items-center">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap">
+                Resume Edit
+              </span>
+            </h3>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-100 uppercase">
+                Resources
+              </h2>
+              <ul className="text-gray-300">
+                <li className="mb-4">
+                  <a href="https://flowbite.com" className="hover:underline">
+                    Flowbite
+                  </a>
+                </li>
+                <li>
+                  <a href="https://undraw.co/" className="hover:underline">
+                    Undraw
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-100 uppercase">
+                Community
+              </h2>
+              <ul className="text-gray-300">
+                <li className="mb-4">
+                  <a
+                    href="https://github.com/Qu1etboy/resume-edit-v2"
+                    className="hover:underline "
+                  >
+                    Github
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-100 uppercase">
+                Legal
+              </h2>
+              <ul className="text-gray-300">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">
+                    Terms &amp; Conditions
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <hr className="my-6 border-gray-800 sm:mx-auto lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-center">
+          <span className="text-sm text-gray-400 sm:text-center">
+            © {new Date().getFullYear()}{" "}
+            <a href="#" className="hover:underline">
+              Resume Edit
+            </a>
+            . All Rights Reserved.
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+}
