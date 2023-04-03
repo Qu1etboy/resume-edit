5b10d67ebdda937e3e142a907048d16cb2d0ec90
Weerawong Vonggatunyu <95042765+Qu1etboy@users.noreply.github.com>
2023-04-04 01:58:36 +0700

    fix: build error in vercel (#3)
    * fix: solution 1
    
    * add try catch

--- pages/api/resume.ts
+++ pages/api/resume.ts
@@ -12,91 +12,101 @@ export default async function handler(
   const prisma = new PrismaClient();
 
   if (req.method === "GET") {
-    const { uid, id } = req.query;
+    try {
+      const { uid, id } = req.query;
 
-    const result = await prisma.resume.findFirst({
-      where: {
-        OR: [
-          {
-            id: id as string | undefined,
-          },
-          {
-            uid: uid as string | undefined,
-          },
-        ],
-      },
-    });
+      const result = await prisma.resume.findFirst({
+        where: {
+          OR: [
+            {
+              id: id as string | undefined,
+            },
+            {
+              uid: uid as string | undefined,
+            },
+          ],
+        },
+      });
 
-    if (result === null) {
+      if (result === null) {
+        return res.status(200).json(result);
+      }
+
+      // // decrypyt resume
+      // const bytes = CryptoJS.AES.decrypt(
+      //   result?.resume,
+      //   process.env.SECRET || "secret key"
+      // );
+
       return res.status(200).json(result);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send("Server error please try again.");
     }
-
-    // // decrypyt resume
-    // const bytes = CryptoJS.AES.decrypt(
-    //   result?.resume,
-    //   process.env.SECRET || "secret key"
-    // );
-
-    return res.status(200).json(result);
   }
 
   if (req.method === "POST") {
-    const session = await getSession({ req });
+    try {
+      const session = await getSession({ req });
 
-    // Unauthorized user can't create or update a resume
-    if (!session || !session.user) {
-      return res.status(403).send("Unauthorized.");
+      // Unauthorized user can't create or update a resume
+      if (!session || !session.user) {
+        return res.status(403).send("Unauthorized.");
+      }
+
+      const { resume }: { resume: Resume } = req.body;
+
+      console.log(resume);
+
+      // encrypt resume before store in db
+      // const encrypted = CryptoJS.AES.encrypt(
+      //   JSON.stringify(resume),
+      //   process.env.SECRET || "secret key"
+      // ).toString();
+
+      const result = await prisma.resume.upsert({
+        where: {
+          uid: session.user.id,
+        },
+        update: {
+          uid: session.user.id,
+          data: {
+            name: resume.data.name,
+            job: resume.data.job,
+            address: resume.data.address,
+            email: resume.data.email,
+            phone: resume.data.phone,
+            contact: resume.data.contact,
+            skill: resume.data.skill,
+            education: resume.data.education,
+            workExp: resume.data.workExp,
+            project: resume.data.project,
+            interest: resume.data.interest,
+          },
+        },
+        create: {
+          uid: session.user.id,
+          data: {
+            name: resume.data.name,
+            job: resume.data.job,
+            address: resume.data.address,
+            email: resume.data.email,
+            phone: resume.data.phone,
+            contact: resume.data.contact,
+            skill: resume.data.skill,
+            education: resume.data.education,
+            workExp: resume.data.workExp,
+            project: resume.data.project,
+            interest: resume.data.interest,
+          },
+        },
+      });
+
+      return res.status(201).json(result);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send("Server error please try again.");
     }
-
-    const { resume }: { resume: Resume } = req.body;
-
-    console.log(resume);
-
-    // encrypt resume before store in db
-    // const encrypted = CryptoJS.AES.encrypt(
-    //   JSON.stringify(resume),
-    //   process.env.SECRET || "secret key"
-    // ).toString();
-
-    const result = await prisma.resume.upsert({
-      where: {
-        uid: session.user.id,
-      },
-      update: {
-        uid: session.user.id,
-        data: {
-          name: resume.data.name,
-          job: resume.data.job,
-          address: resume.data.address,
-          email: resume.data.email,
-          phone: resume.data.phone,
-          contact: resume.data.contact,
-          skill: resume.data.skill,
-          education: resume.data.education,
-          workExp: resume.data.workExp,
-          project: resume.data.project,
-          interest: resume.data.interest,
-        },
-      },
-      create: {
-        uid: session.user.id,
-        data: {
-          name: resume.data.name,
-          job: resume.data.job,
-          address: resume.data.address,
-          email: resume.data.email,
-          phone: resume.data.phone,
-          contact: resume.data.contact,
-          skill: resume.data.skill,
-          education: resume.data.education,
-          workExp: resume.data.workExp,
-          project: resume.data.project,
-          interest: resume.data.interest,
-        },
-      },
-    });
-
-    return res.status(201).json(result);
   }
 
   return res.status(405).send("Method not allowed.");
